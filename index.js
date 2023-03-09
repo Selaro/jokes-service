4f3785cff1e68e2a2ba625cb508d73d54850aa3f
Edin <moralesedin01@gmail.com>
2023-03-09 15:52:30 -0500

    created get routes for jokes with filters

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const { Op } = require('sequelize')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = await Joke.findAll({
+    const {tags, content} = req.query;
+    const where = {};
 
+    if(tags) where.tags = {[Op.like]: `%${tags}%`};
+    if(content) where.joke = {[Op.like]: `%${content}%`};
+    // TODO - filter the jokes by tags and content
+
+    const jokes = await Joke.findAll({
+      where
     });
     res.send(jokes);
   } catch (error) {
